f5553d95ed473a24befc3a8547e2a0f6ac34cd5d
f2firdaus <firdausnaseem@gmail.com>
2023-03-05 12:18:01 +0530

    node Assign

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server started');
+});
